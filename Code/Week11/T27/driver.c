cfee192f5d5c1f5789a8aad3ce55893d41d0d605
chilatticefi <chi@latticefi.com>
2024-11-24 10:22:32 +0000

    TODO:2 challenges

--- Code/Week11/T27/driver.c
+++ Code/Week11/T27/driver.c
@@ -22,7 +22,12 @@ int main(void)
    assert(dict_addword(d, "cart"));
    assert(dict_addword(d, "part"));
    assert(dict_nodecount(d)==9);
+   int cnt = dict_nodecount(d);
+   printf("cnt:%i\n", cnt);
+   int wd_count = dict_wordcount(d);
    assert(dict_wordcount(d)==3);
+   printf("wd_count:%i\n", wd_count);
+
    assert(dict_mostcommon(d)==1);
    dict* q1 = dict_spell(d, "car");
    dict* q2 = dict_spell(d, "part");
@@ -35,86 +40,86 @@ int main(void)
    assert(strcmp(str, "t")==0);
    dict_free(&d);
 
-/* A slightly different example */
-   d = dict_init();
-   // Should be a non-NULL address
-   assert(d);
-   // Top node always exists
-   assert(dict_nodecount(d)==1);
-   assert(dict_wordcount(d)==0);
-   // Can't add a NULL string
-   assert(dict_spell(d, NULL)==NULL);
+// /* A slightly different example */
+//    d = dict_init();
+//    // Should be a non-NULL address
+//    assert(d);
+//    // Top node always exists
+//    assert(dict_nodecount(d)==1);
+//    assert(dict_wordcount(d)==0);
+//    // Can't add a NULL string
+//    assert(dict_spell(d, NULL)==NULL);
 
-   // 'car' isn't in the tree
-   assert(dict_spell(d, "car")==NULL);
-   // So add it
-   assert(dict_addword(d, "car"));
-   // It's there now
-   assert(dict_spell(d, "car"));
-   // Top node + 'c' + 'a' + 'r'
-   assert(dict_nodecount(d)==4);
-   assert(dict_wordcount(d)==1);
+//    // 'car' isn't in the tree
+//    assert(dict_spell(d, "car")==NULL);
+//    // So add it
+//    assert(dict_addword(d, "car"));
+//    // It's there now
+//    assert(dict_spell(d, "car"));
+//    // Top node + 'c' + 'a' + 'r'
+//    assert(dict_nodecount(d)==4);
+//    assert(dict_wordcount(d)==1);
 
-   // carted isn't in there
-   assert(dict_spell(d, "carted")==NULL);
-   // Put it in
-   assert(dict_addword(d, "carted"));
-   // It is now
-   assert(dict_spell(d, "carted"));
-   // 'car' -> 'carted' is 3 extra nodes
-   assert(dict_nodecount(d)==7);
-   assert(dict_wordcount(d)==2);
+//    // carted isn't in there
+//    assert(dict_spell(d, "carted")==NULL);
+//    // Put it in
+//    assert(dict_addword(d, "carted"));
+//    // It is now
+//    assert(dict_spell(d, "carted"));
+//    // 'car' -> 'carted' is 3 extra nodes
+//    assert(dict_nodecount(d)==7);
+//    assert(dict_wordcount(d)==2);
 
-   // Cart isn't there as a terminal word
-   assert(dict_spell(d, "cart")==NULL);
-   // Put it in (case-insensitive)
-   assert(dict_addword(d, "Cart"));
-   // It's now there
-   assert(dict_spell(d, "cart"));
-   // No new nodes were created
-   assert(dict_nodecount(d)==7);
-   assert(dict_wordcount(d)==3);
+//    // Cart isn't there as a terminal word
+//    assert(dict_spell(d, "cart")==NULL);
+//    // Put it in (case-insensitive)
+//    assert(dict_addword(d, "Cart"));
+//    // It's now there
+//    assert(dict_spell(d, "cart"));
+//    // No new nodes were created
+//    assert(dict_nodecount(d)==7);
+//    assert(dict_wordcount(d)==3);
 
-   // Cart'd isn't there as a terminal word
-   assert(dict_spell(d, "cart'd")==NULL);
-   // Put it in
-   assert(dict_addword(d, "cart'd"));
-   // It's now there
-   assert(dict_spell(d, "Cart'd"));
-   // New nodes were created
-   assert(dict_nodecount(d)==9);
-   assert(dict_wordcount(d)==4);
+//    // Cart'd isn't there as a terminal word
+//    assert(dict_spell(d, "cart'd")==NULL);
+//    // Put it in
+//    assert(dict_addword(d, "cart'd"));
+//    // It's now there
+//    assert(dict_spell(d, "Cart'd"));
+//    // New nodes were created
+//    assert(dict_nodecount(d)==9);
+//    assert(dict_wordcount(d)==4);
 
-   // Only one of each word at the moment
-   assert(dict_mostcommon(d)==1);
-   // Just increments the freq variable
-   assert(dict_addword(d, "cart"));
-   // Now 'cart' has been added twice 
-   assert(dict_mostcommon(d)==2);
-   // But no new nodes were created
-   assert(dict_nodecount(d)==9);
-   // Fifth word
-   assert(dict_wordcount(d)==5);
+//    // Only one of each word at the moment
+//    assert(dict_mostcommon(d)==1);
+//    // Just increments the freq variable
+//    assert(dict_addword(d, "cart"));
+//    // Now 'cart' has been added twice 
+//    assert(dict_mostcommon(d)==2);
+//    // But no new nodes were created
+//    assert(dict_nodecount(d)==9);
+//    // Fifth word
+//    assert(dict_wordcount(d)==5);
 
-   // Pointer to the bottom of cart...
-   dict* p = dict_spell(d, "cart");
-   assert(p);
+//    // Pointer to the bottom of cart...
+//    dict* p = dict_spell(d, "cart");
+//    assert(p);
 
-   // Add some new words
-   dict_addword(d, "par");
-   dict_addword(d, "part");
-   dict_addword(d, "parted");
-   dict_addword(d, "parter");
-   // 7 new nodes: p+a+r+t+e+d+r
-   assert(dict_nodecount(d)==16);
-   // 5 car prefixed + 4 par prefixed
-   assert(dict_wordcount(d)==9);
+//    // Add some new words
+//    dict_addword(d, "par");
+//    dict_addword(d, "part");
+//    dict_addword(d, "parted");
+//    dict_addword(d, "parter");
+//    // 7 new nodes: p+a+r+t+e+d+r
+//    assert(dict_nodecount(d)==16);
+//    // 5 car prefixed + 4 par prefixed
+//    assert(dict_wordcount(d)==9);
    dict_free(&d);
-   assert(d==NULL);
+   // assert(d==NULL);
 
 /* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
 /* Basic functions, but lots of data from file */
-
+/*
    #define DICTFILES 3
    // unique words : wordle has no apostrophes
    // but english_65197 does
@@ -155,7 +160,8 @@ int main(void)
       assert(dict_wordcount(d)==wc);
    }
 
-/* Leave this commented out to begin with
+
+// Leave this commented out to begin with
 // !!!!!!!!!!!!!!!!!!!!!!!
 // 10% : DICT_AUTOCOMPLETE
    d = dict_init();
@@ -208,12 +214,12 @@ int main(void)
    // 5 moves to top, 5 moves back down again
    assert(dict_cmp(p1, p2)==10);
    dict_free(&d);
-*/
+
 
    // Free up all those dictionaries
    for(int i=0; i<DICTFILES; i++){
       dict_free(&dcts[i]);
    }
-
+*/
    return 0;
 }
--- Code/Week11/T27/t27.c
+++ Code/Week11/T27/t27.c
@@ -2,40 +2,156 @@
 
 // 'Internal' function prototypes 
 // ...
+dict* create_node(dict* p);
+void print_dict(dict* p);
 
 dict* dict_init(void)
 {
+  struct dict* node = malloc(sizeof(dict));
+  for(int i=0; i<ALPHA; i++){
+    node->dwn[i] = NULL;
+  }
+  node->up = NULL;
+  node->terminal = false;
+  node->freq = 0;
+  return node;
 }
 
+/**
+ * 0:a, 1:b, 2:c ... 25:z, 26:'
+ */
 bool dict_addword(dict* p, const char* wd)
 {
+  struct dict* curr = p;
+  for(int i=0; wd[i] != '\0'; i++){
+    // trans all to small case
+    char c = tolower(wd[i]);
+    printf("c:%c\n", c);  // Output the lowercase character
+    int index = c - 'a'; // a-z: 0-25
+    printf("index:%i\n", index);
+    if(curr->dwn[index] == NULL){
+      printf("NULL\n");
+      curr->dwn[index] = create_node(curr);
+      curr->dwn[index]->up = curr;
+    }
+    
+    printf("----------------------\n");
+    curr = curr->dwn[index]; // move to next node
+    
+  }
+  curr->terminal = true; // last node, terminal true.
+  curr->freq++;
+  print_dict(curr);
+  return true;
+}
+
+void print_dict(struct dict* d) {
+    if (d == NULL) return;
+    printf("Terminal: %s\n", d->terminal? "true":"false");
+    printf("Frequency: %d\n", d->freq);
+    // Print the 'up' pointer (parent)
+    if (d->up != NULL) {
+        printf("Parent exists:\n");
+        print_dict(d->up);  // Recursively print the parent
+    } else {
+        printf("No parent (this is the root).\n");
+    }
+        
+    for (int i = 0; i < ALPHA; i++) {
+        // Print down pointers (children)
+        if (d->dwn[i] != NULL) {
+            printf("Child[%d] exists.\n", i);
+        }
+    }
+    
+}
+
+
+dict* create_node(dict* p)
+{
+  dict* node = dict_init();
+  return node;
 }
 
 void dict_free(dict** d)
 {
+  if(*d){
+    free(*d);
+    *d = NULL; // prevent wild pointers
+  }
 }
 
 int dict_wordcount(const dict* p)
 {
+  if(!p) return 0;
+  int wd_cnt = 0;
+  if(p->terminal){
+    wd_cnt += 1;
+  }
+  for(int i=0; i<ALPHA; i++){
+    wd_cnt += dict_wordcount(p->dwn[i]);
+  }
+  return wd_cnt;
 }
 
 int dict_nodecount(const dict* p)
 {
+  // only access nodes that are not NULL
+  if(p==NULL){
+    return 0; 
+  }
+  int cnt = 1;
+  for(int i=0; i<ALPHA; i++){
+    cnt += dict_nodecount(p->dwn[i]);
+  }
+
+  return cnt;
 }
 
 dict* dict_spell(const dict* p, const char* str)
 {
+  if(p == NULL) return NULL;
+  dict* curr = p;
+  int len = strlen(str);
+  for(int i=0; i<len; i++){
+    char c = tolower(str[i]);
+    int index = c - 'a';
+    if(!curr->dwn[index]) return NULL;
+    if(i+1 == len && curr->terminal) return curr;
+    curr = curr->dwn[index]; //move to next word;
+  }
 }
 
 int dict_mostcommon(const dict* p)
 {
+  if(!p) return 0;
+  int max_freq = 0;
+  if(p->terminal){
+    max_freq = p->freq;
+  }
+
+  for(int i=0; i<ALPHA; i++){
+    int child_freq = dict_mostcommon(p->dwn[i]);
+    if(child_freq > max_freq){
+      max_freq = child_freq;
+    }
+  }
+  return max_freq;
 }
 
+//TODO
 // CHALLENGE1
 unsigned dict_cmp(dict* p1, dict* p2)
 {
+  // p1: c->a->r
+  // p2: p->a->r->t
+  if(!p1 || !p2) return 0; // unsure if one of them is null
+  // what should i return. return p1+p2 step or not cmp (return 0).
+  
+
 }
 
+//TODO
 // CHALLENGE2
 void dict_autocomplete(const dict* p, const char* wd, char* ret)
 {
